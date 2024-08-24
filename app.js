ce8ddd80a76b1a70e8fb49dce08ebce124f2b52e
Naleef <mohamednaleef@gmail.com>
2024-08-24 19:59:31 +0530

    more directory issues

--- app.js
+++ app.js
@@ -699,7 +699,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/hospital-PWA/service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered successfully:', registration.scope);
             })
